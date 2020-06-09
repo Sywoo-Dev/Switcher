5c5939d63577ab842dbd08081d126254684eec2a
Sywoo <contact@lenglet.pro>
2020-06-09 21:17:17 +0200

    First and Last commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.getKey(KeyC))
+        {
+            Debug.log("Heuuu");
+        }
+    }
+}
